d3a47dde73700e4ae6712a471929018d2716eb4c
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-04 14:35:00 -0400

    AG via `<leader>ag`

--- tag-nvim/config/nvim/lua/plugins/telescope-ag.lua
+++ tag-nvim/config/nvim/lua/plugins/telescope-ag.lua
@@ -1,4 +1,15 @@
 return {
 	"kelly-lin/telescope-ag",
 	dependencies = { "nvim-telescope/telescope.nvim" },
+	keys = {
+        {
+            "<leader>ag",
+            function()
+                require('telescope.builtin').live_grep({ 
+                    default_text = vim.fn.expand('<cword>') 
+                })
+            end,
+            desc = "Live grep search"
+        },
+    },
 }
