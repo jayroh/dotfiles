c4cfb5a91afdf79159fc11a1a82280810cd2d236
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-15 08:44:26 -0400

    Update completion config
    1. show documentation when completion dialog is open. And ...
    2. explicitly lay out the sources and my preferred order

--- tag-nvim/config/nvim/lua/plugins/completion.lua
+++ tag-nvim/config/nvim/lua/plugins/completion.lua
@@ -9,10 +9,14 @@ return {
 
     appearance = { nerd_font_variant = 'mono' },
 
-    completion = { documentation = { auto_show = false } },
+    completion = { documentation = { auto_show = true } },
 
     fuzzy = { implementation = "prefer_rust_with_warning" },
 
 	signature = { enabled = true },
+
+	sources = {
+		default = { 'lsp', 'path', 'snippets', 'buffer' }
+	}
   },
 }
