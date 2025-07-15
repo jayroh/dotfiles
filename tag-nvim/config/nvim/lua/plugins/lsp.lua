a2b3b14be8e58c82a8bd27f39131acfe862282f8
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-14 22:03:05 -0400

    Set up lsp's - mason, et al

---
+++ tag-nvim/config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,44 @@
+return {
+	'williamboman/mason.nvim',
+
+	dependencies = {
+		'williamboman/mason-lspconfig.nvim',
+		'neovim/nvim-lspconfig',
+	},
+
+	opts = {
+		servers = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			},
+			rubocop = {},
+			solargraph = {},
+			docker_compose_language_service = {},
+			dockerls = {},
+			tailwindcss = {},
+		},
+	},
+	config = function(_, opts)
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			ensure_installed = {"lua_ls", "eslint"}
+		})
+
+		for server, config in pairs(opts.servers) do
+			vim.lsp.config(server, config)
+			vim.lsp.enable(server)
+		end
+
+		vim.diagnostic.config({
+			virtual_text = true,
+			underline = true,
+		})
+	end,
+}
