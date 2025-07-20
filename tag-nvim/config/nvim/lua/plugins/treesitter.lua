e7ed48b0cf48c11645335c1c9c806641f175f01c
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-20 16:09:56 -0400

    Get treesitter further configured

---
+++ tag-nvim/config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,68 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"nvim-treesitter/nvim-treesitter-context",
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"ruby",
+				"html",
+				"javascript",
+				"typescript",
+				"tsx",
+				"lua",
+				"vim",
+				"css",
+				"json",
+				"yaml",
+				"markdown",
+				"bash",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = "<C-s>",
+					node_decremental = "<M-space>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
+				},
+			},
+		})
+
+		require("treesitter-context").setup({
+			max_lines = 3,
+			trim_scope = "outer",
+			mode = "cursor",
+		})
+
+		require("ts_context_commentstring").setup({
+			enable_autocmd = false,
+		})
+	end,
+}
