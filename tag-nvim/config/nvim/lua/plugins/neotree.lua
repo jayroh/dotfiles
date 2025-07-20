ae136b275e42b7ff59a4c745a86af1ecd0fc2f51
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-19 20:03:33 -0400

    style: neotree configuration code indentation adjustment

--- tag-nvim/config/nvim/lua/plugins/neotree.lua
+++ tag-nvim/config/nvim/lua/plugins/neotree.lua
@@ -1,21 +1,15 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-  },
-  lazy = false, -- neo-tree will lazily load itself
-  ---@module "neo-tree"
-  ---@type neotree.Config?
-  opts = {
-    -- fill any relevant options here
-  },
-  keys = {
-    { "<leader>nt", "<cmd>Neotree toggle filesystem<cr>", desc = "Toggle Neo-tree filesystem" },
-    { "<leader>nb", "<cmd>Neotree toggle buffers<cr>", desc = "Toggle Neo-tree buffers" },
-    { "<leader>be", "<cmd>Neotree toggle buffers<cr>", desc = "Toggle Neo-tree buffers" },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	keys = {
+		{ "<leader>nt", "<cmd>Neotree toggle filesystem<cr>", desc = "Toggle Neo-tree filesystem" },
+		{ "<leader>nb", "<cmd>Neotree toggle buffers<cr>", desc = "Toggle Neo-tree buffers" },
+		{ "<leader>be", "<cmd>Neotree toggle buffers<cr>", desc = "Toggle Neo-tree buffers" },
+	},
 }
