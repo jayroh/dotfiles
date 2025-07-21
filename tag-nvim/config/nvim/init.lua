fd4ddeedb39b6853f03ac74a210252e228ff5804
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-21 11:11:53 -0400

    Set colorcolumn to 100

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -2,7 +2,7 @@
 vim.opt.exrc = true
 
 -- line number
-vim.opt.number = true         -- Show absolute line numbers
+vim.opt.number = true -- Show absolute line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -20,34 +20,36 @@ vim.g.maplocalleader = "\\"
 -- This will help with autocomplete as well. Trying to auto complete "is" by
 -- default would do nothing, but now it will try to autocomplete "is-keyword".
 -- This is super helpful for when you are editing (S)CSS selectors.
-vim.opt.iskeyword:append('-')
+vim.opt.iskeyword:append("-")
 
 -- vim-plug loads all the filetype, syntax and colorscheme files, so turn them on
 -- _after_ loading plugins.
-vim.cmd('filetype plugin indent on')
+vim.cmd("filetype plugin indent on")
 vim.opt.sw = 4
 vim.opt.ts = 4
-vim.cmd('syntax enable')
+vim.cmd("syntax enable")
 
 -- vim-textobj-rubyblock requires that the matchit.vim plugin is enabled.
-vim.cmd('runtime macros/matchit.vim')
+vim.cmd("runtime macros/matchit.vim")
 
-vim.cmd('highlight Comment cterm=italic gui=italic')
+vim.cmd("highlight Comment cterm=italic gui=italic")
 
 -- config python host
-vim.g.python3_host_prog = vim.fn.exepath('python3')
+vim.g.python3_host_prog = vim.fn.exepath("python3")
 
 -- config ruby host
-vim.g.ruby_host_prog = vim.fn.exepath('neovim-ruby-host')
+vim.g.ruby_host_prog = vim.fn.exepath("neovim-ruby-host")
 
 -- snippet location
-vim.g.vsnip_snippet_dir = '~/.dotfiles/tag-nvim/snippets'
+vim.g.vsnip_snippet_dir = "~/.dotfiles/tag-nvim/snippets"
 
 -- ignore perl
 vim.g.loaded_perl_provider = 0
 
 -- disable automatic folding
 vim.opt.foldenable = false
+
+vim.opt.colorcolumn = "100"
 
 -- load lazy plugins
 require("config.lazy")
