69672d1a314206ed613575ca82656d9f06c35c65
Joel Oliveira <joel.oliveira@gmail.com>
2025-07-21 21:19:14 -0400

    ts and js uses eslint and prettier on save w/conform

--- tag-nvim/config/nvim/lua/plugins/autoformat.lua
+++ tag-nvim/config/nvim/lua/plugins/autoformat.lua
@@ -8,6 +8,10 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				ruby = { "rubocop" },
+				javascript = { "eslint", "prettier" },
+				typescript = { "eslint", "prettier" },
+				javascriptreact = { "eslint", "prettier" },
+				typescriptreact = { "eslint", "prettier" },
 			},
 			formatters = {
 				rubocop = {
@@ -20,6 +24,7 @@ return {
 			format_on_save = {
 				timeout_ms = 5000,
 				lsp_format = "fallback",
+				stop_after_first = false, -- Run both eslint_d AND prettier
 			},
 		})
 	end,
