5867e942ec6bbd96bb46d5e79ed07da059e82717
Joel Oliveira <joel.oliveira@gmail.com>
2025-09-06 09:52:34 -0400

    Update completions
    Re-order the completion prioritization

--- tag-nvim/config/nvim/lua/plugins/completion.lua
+++ tag-nvim/config/nvim/lua/plugins/completion.lua
@@ -1,22 +1,22 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
 
-  version = '1.*',
+	version = "1.*",
 
-  opts = {
-    keymap = { preset = 'super-tab' },
+	opts = {
+		keymap = { preset = "super-tab" },
 
-    appearance = { nerd_font_variant = 'mono' },
+		appearance = { nerd_font_variant = "mono" },
 
-    completion = { documentation = { auto_show = true } },
+		completion = { documentation = { auto_show = true } },
 
-    fuzzy = { implementation = "prefer_rust_with_warning" },
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 
-	signature = { enabled = true },
+		signature = { enabled = true },
 
-	sources = {
-		default = { 'lsp', 'path', 'snippets', 'buffer' }
-	}
-  },
+		sources = {
+			default = { "path", "snippets", "buffer", "lsp" },
+		},
+	},
 }
