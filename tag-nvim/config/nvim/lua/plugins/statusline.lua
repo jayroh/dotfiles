8248ce2aa2d98c6b86484086adceeabbf9ed1d18
Joel Oliveira <joel.oliveira@gmail.com>
2025-09-06 11:10:28 -0400

    Add filetype to statusline

--- tag-nvim/config/nvim/lua/plugins/statusline.lua
+++ tag-nvim/config/nvim/lua/plugins/statusline.lua
@@ -1,16 +1,16 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require('lualine').setup {
+		require("lualine").setup({
 			sections = {
-				lualine_a = { 'mode' },
-				lualine_b = { 'branch', 'diff', 'diagnostics' },
-				lualine_c = { 'filename' },
-				lualine_x = { '' },
-				lualine_y = { '' },
-				lualine_z = { 'datetime' },
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "" },
+				lualine_y = { "" },
+				lualine_z = { "filetype", "datetime" },
 			},
-		}
+		})
 	end,
 }
