caeaf1a7bcdbbf567fb5bb1a0b70bd05256d8b47
Joel Oliveira <joel.oliveira@gmail.com>
2026-08-12 20:53:16 -0400

    Start getting herdr set up

--- tag-nvim/config/nvim/lua/plugins/tmux.lua
+++ tag-nvim/config/nvim/lua/plugins/tmux.lua
@@ -1,23 +1,21 @@
+-- vim-herdr-navigation supersedes vim-tmux-navigator's mappings when present:
+-- it handles herdr panes and falls back to tmux itself. Guarded so machines
+-- without the herdr plugin keep vim-tmux-navigator's own mappings.
+local herdr_nav = vim.fn.expand("~/.config/herdr/vim-herdr-navigation/editor/nvim.lua")
+local has_herdr_nav = vim.uv.fs_stat(herdr_nav) ~= nil
+
 return {
 	{
 		"christoomey/vim-tmux-navigator",
-
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-			"TmuxNavigatorProcessList",
-		},
-
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
+		lazy = false,
+		init = function()
+			vim.g.tmux_navigator_no_mappings = has_herdr_nav and 1 or nil
+		end,
+		config = function()
+			if has_herdr_nav then
+				dofile(herdr_nav)
+			end
+		end,
 	},
 	{
 		"christoomey/vim-tmux-runner",
